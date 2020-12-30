fcdf7fb4748d78ee05c3e230a262bfbec7df46fa
高国权 <gaoguoquan8273@dingtalk.com>
2020-12-30 10:41:53 +0800

    创建项目

---
+++ src/main/java/com/ggq/worldtree/WorldtreeApplication.java
@@ -0,0 +1,13 @@
+package com.ggq.worldtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggq/worldtree/WorldtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggq.worldtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
